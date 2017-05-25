e7e089aeac683129812dda42d7ceacb33a7be203
willo <william.olner@gmail.com>
2017-05-25 13:28:46 +0100

    added get realworld

--- src/binding.cc
+++ src/binding.cc
@@ -130,9 +130,9 @@ void *readFromFIFO(void *ypr_void_ptr) {
       mpu.dmpGetAccel(&aa, fifoBuffer);
       mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
 
-      ypr_ptr[10] = aa.x;
-      ypr_ptr[11] = aa.y;
-      ypr_ptr[12] = aa.z;
+      ypr_ptr[10] = 1;
+      ypr_ptr[11] = 1;
+      ypr_ptr[12] = 1;
       ;
       //printf("ypr  %7.2f %7.2f %7.2f\n", ypr[0] * 180/M_PI, ypr[1] * 180/M_PI, ypr[2] * 180/M_PI);
       ypr_ptr[0] = ypr[0] * 180/M_PI;
